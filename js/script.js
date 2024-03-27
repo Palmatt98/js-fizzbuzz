cf9fca3797d558b8297caafea2be5c0965b793cc
Palmatt98 <palmierimatteo98@gmail.com>
2024-03-27 22:48:32 +0100

    consol log compoleted

--- js/script.js
+++ js/script.js
@@ -4,13 +4,22 @@ for (let i = 1; i <= 100; i++) {
 	console.log(i);
 	let bgClass;
 	if (i % 2 === 0) {
-		bgClass = "even";
+		bgClass = "pari";
 		console.log(bgClass);
 	} else {
-		bgClass = "odd";
+		bgClass = "dispari";
 		console.log(bgClass);
 	}
-	
+	if (i % 3 === 0 && i % 5 === 0) {
+		bgClass = "fizzbuzz";
+		console.log("fizzbuzz");
+	} else if (i % 3 === 0) {
+		bgClass = "fizz";
+		console.log("fizz");
+	} else if (i % 5 === 0) {
+		bgClass = "buzz"
+		console.log("buzz");
+	}
 
 	let boxElem = `<div class="box ${bgClass}">${i}</div> `;
 	rowElement.innerHTML += boxElem;
